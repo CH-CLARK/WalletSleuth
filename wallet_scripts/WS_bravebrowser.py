f4fee7a01c2ed22948bc4db7da18000c43011407
ConnorHClark <connor@henriclark.com>
2022-12-13 23:03:33 +0000

    Update WS_bravebrowser.py

--- wallet_scripts/WS_bravebrowser.py
+++ wallet_scripts/WS_bravebrowser.py
@@ -22,7 +22,64 @@ def bravebrowser_dump(ask_dir, output_dir):
     bravebrowser_output = []
 
     if default_list:
-        print("DO DEFAULT LOCATION")
+        default_user_location = bravebrowser_userdata + '/Default/Preferences'
+
+        with open(default_user_location, 'r') as def_user_loc_pref:
+            read_def = def_user_loc_pref.read()
+
+            json_obj = json.loads(read_def)
+            brave_obj = json_obj['brave']
+            wallet_obj = brave_obj['wallet']
+            keyrings_obj = wallet_obj['keyrings']
+
+            default_obj = keyrings_obj['default']
+            default_metas_obj = default_obj['account_metas']
+
+            filecoin_obj = keyrings_obj['filecoin']
+            filecoin_metas_obj = filecoin_obj['account_metas']
+
+            solana_obj = keyrings_obj['solana']
+            solana_metas_obj = solana_obj['account_metas']
+
+            if default_obj:
+                for i in default_metas_obj:
+                    default_num_wallets = (len(default_metas_obj))
+                    
+                for c in range(0, default_num_wallets):
+                    num = str(c)
+                    meta_objs = "m/44'/60'/0'/0/" + num
+                    address_obj = default_metas_obj[meta_objs]
+                    default_addresses = address_obj['account_address']      
+                    default_output = 'VARIOUS - See Documention!', default_addresses,  'Brave Browser Wallet', default_user_location
+                    bravebrowser_output.append(default_output)
+
+            if filecoin_obj:
+                for i in filecoin_metas_obj:
+                    filecoin_num_wallets = (len(filecoin_metas_obj))
+
+                for c in range(0, filecoin_num_wallets):
+                    num = str(c)
+                    meta_objs = "m/44'/461'/0'/0/" + num
+                    address_obj = filecoin_metas_obj[meta_objs]
+                    filecoin_addresses = address_obj['account_address']      
+                    filecoin_output = 'VARIOUS - See Documention!', filecoin_addresses,  'Brave Browser Wallet', default_user_location
+                    bravebrowser_output.append(filecoin_output)
+            
+            if solana_obj:
+                for i in solana_metas_obj:
+                    solana_num_wallets = (len(solana_metas_obj))
+
+                for c in range(0, solana_num_wallets):
+                    num = str(c)
+                    meta_objs = "m/44'/501'/" + num + "'/0'"
+                    address_obj = solana_metas_obj[meta_objs]
+                    solana_addresses = address_obj['account_address']      
+                    solana_ouput = 'VARIOUS - See Documention!', solana_addresses,  'Brave Browser Wallet', default_user_location
+                    bravebrowser_output.append(solana_ouput)
+
+        with open(output_dir + '/' + 'WalletSleuth_log.txt', 'a') as log_file:
+            log_file.write('ACTION: (BRAVE BROWSER) - Addresses Identified in Default.\n')
+
 
     if profiles_list:
         print("DO PROFILES LOCATIONS")
