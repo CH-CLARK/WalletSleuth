92dbd4bce532e3ca5d950495da8b590638d5e979
CH-CLARK <connor@henriclark.com>
2026-04-26 21:49:43 +0100

    Wallet Sleuth CLI
    - user_config.py cretaed to store user varibles (I/O direcotries and OS)
    - Subsequent changes to 'routes/config'.
    - working 'all_desktop' CLI functionality

--- wallet_sleuth_cli.py
+++ wallet_sleuth_cli.py
@@ -1,9 +1,15 @@
-from wallet_sleuth import title
-from routes.config import configuration
-from utils.pyinstaller_utils import resource_path
-from model.load_wallets import load_wallet_functions
+# generic imports
 import argparse
+import os
+import csv
 
+#wallet sleuth imports
+from wallet_sleuth import title
+from utils.user_config import configuration
+from utils.pyinstaller_utils import resource_path
+from utils.wallet_utils import normalize_type
+from model.load_wallets import load_wallet_functions
+from wallet_scripts.desktop_wallets.All_Desktop_Wallets import all_desktop_wallets
 
 def main():
     print(title)
@@ -30,13 +36,13 @@ def main():
                         action='store_true', 
                         help='Run all desktop wallet scripts')
     
-    # parser.add_argument('--browser_wallets', 
-    #                     action='store_true', 
-    #                     help='Run all browser wallet scripts')
+    parser.add_argument('--browser_wallets', 
+                        action='store_true', 
+                        help='Run all browser wallet scripts')
     
-    # parser.add_argument('--all_wallets', 
-    #                     action='store_true', 
-    #                     help='Run all wallet scripts')
+    parser.add_argument('--all_wallets', 
+                        action='store_true', 
+                        help='Run all wallet scripts')
 
     args = parser.parse_args()
 
@@ -44,31 +50,59 @@ def main():
     output_path = args.o
     os_selection = 'windows' if args.os == 'WIN' else 'mac'
     desktop_wallets = args.desktop_wallets
-
-    #for tetsing
-    # print(f'Profile Path   : {profile_path}')
-    # print(f'Output Path    : {output_path}')
-    # print(f'OS Selection   : {os_selection}')
-    # print(f'Desktop Wallets: {desktop_wallets}')
-
-    #recereating the config locally in the cli version
-    class Config:
-        OS_SELECTION = None
-        DIRECTORY_PATH = None
-        OUTPUT_PATH = None
-
-    configuration = Config()
+    browser_wallets = args.browser_wallets
+    all_wallets = args.all_wallets
+ 
     configuration.DIRECTORY_PATH = profile_path
     configuration.OUTPUT_PATH = output_path
     configuration.OS_SELECTION = os_selection
 
+    print('Wallet Sleuth - Scan Settings')
+    print('======================================')
+    print(f'User Directory: {configuration.DIRECTORY_PATH}')
+    print(f'Output Directory: {configuration.OUTPUT_PATH}')
+    print(f'Operating System: {configuration.OS_SELECTION}')
+    print('======================================')
 
-    print('config variables')
-    print('======================================')
-    print(configuration.OS_SELECTION)
-    print(configuration.DIRECTORY_PATH)
-    print(configuration.OUTPUT_PATH)
-    print('======================================')
+
+    #run wallet_scripts
+    func_list = []
+    header = 'Type', 'Currency', 'Address/Transaction', 'Wallet', 'Path'
+
+    if desktop_wallets:
+        all_desktop_wallets()
+        wallet_functions = load_wallet_functions([resource_path('wallet_scripts/desktop_wallets')])
+        func_list = [i for i in wallet_functions if i != 'all_desktop_wallets']
+
+    elif browser_wallets:
+        print('this will do browser wallets')
+
+    elif(all_wallets):
+        print('this will do all wallets')
+    
+    master_file = os.path.join(output_path, 'wallet_sleuth_output.csv')
+
+    with open(master_file, 'w', newline='', encoding='utf-8') as master_csv:
+        writer = csv.DictWriter(master_csv, fieldnames=list(header))
+        writer.writeheader()
+
+        for function in func_list:
+            file_name = f'{function}_ws_output.csv'
+            file_path = os.path.join(output_path, file_name)
+            try:
+                with open(file_path, 'r', newline='', encoding='utf-8') as f:
+                    reader = csv.DictReader(f)
+                    for row in reader:
+                        row = {key: row.get(key, '') for key in header}
+                        row['Currency'] = normalize_type(row.get('Currency', ''))
+                        if not row:
+                            continue
+                        writer.writerow(row)
+            except Exception:
+                pass
+
+    print(f'Scan Complete!')
+
 
 if __name__ == "__main__":
     main()
